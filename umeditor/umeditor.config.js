d9e4f918e9e1dce243b750e5a1e0b7289c125a02
yibo7 <58937236+yibo7@users.noreply.github.com>
2023-09-01 09:53:49 +0800

    config

--- umeditor/umeditor.config.js
+++ umeditor/umeditor.config.js
@@ -140,8 +140,8 @@ etpl.config({
         UMEDITOR_HOME_URL : URL
 
         //图片上传配置区
-        ,imageUrl:URL+"php/imageUp.php"             //图片上传提交地址
-        ,imagePath:URL + "php/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
+        ,imageUrl:URL+"api/upfile?t=ume"   //图片上传提交地址
+        ,imagePath:""                     //URL + "upfile"图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
         ,imageFieldName:"upfile"                   //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
 
 
@@ -151,7 +151,7 @@ etpl.config({
             'insertorderedlist insertunorderedlist | selectall cleardoc paragraph | fontfamily fontsize' ,
             '| justifyleft justifycenter justifyright justifyjustify |',
             'link unlink | emotion image video  | map',
-            '| horizontal print preview ', 'drafts', 'formula'
+            '| horizontal print preview fullscreen', 'drafts', 'formula'
         ]
 
         //语言配置项,默认是zh-cn。有需要的话也可以使用如下这样的方式来自动多语言切换，当然，前提条件是lang文件夹下存在对应的语言文件：
@@ -184,11 +184,11 @@ etpl.config({
 
         //,textarea:'editorValue' // 提交表单时，服务器获取编辑器提交内容的所用的参数，多实例时可以给容器name属性，会将name给定的值最为每个实例的键值，不用每次实例化的时候都设置这个值
 
-        ,focus:true //初始化时，是否让编辑器获得焦点true或false
+        //,focus:false //初始化时，是否让编辑器获得焦点true或false
 
         //,autoClearEmptyNode : true //getContent时，是否删除空的inlineElement节点（包括嵌套的情况）
 
-        ,fullscreen : true //是否开启初始化时即全屏，默认关闭
+        //,fullscreen : false //是否开启初始化时即全屏，默认关闭
 
         //,readonly : false //编辑器初始化结束后,编辑区域是否是只读的，默认是false
 
